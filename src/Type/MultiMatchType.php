5a9b38a87dcf62a9cd0976d8ded1da562ff8cb6c
David Meybohm <dmeybohm@gmail.com>
2018-08-28 00:35:35 -0400

    Add MultiMatch

---
+++ src/Type/MultiMatchType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Best\ElasticSearch\Hercules\Type;
+
+use Best\ElasticSearch\Hercules\Traits\StringableTrait;
+
+class MultiMatchType
+{
+    use StringableTrait;
+    
+    /**
+     * Return a best_fields multi match type
+     *
+     * @return static
+     */
+    public static function bestFields()
+    {
+        return new static('best_fields');
+    }
+    
+    /**
+     * Return a most_fields multi match type
+     *
+     * @return static
+     */
+    public static function mostFields()
+    {
+        return new static('most_fields');
+    }
+
+    /**
+     * Return a cross_fields multi match type
+     *
+     * @return static
+     */
+    public static function crossFields()
+    {
+        return new static('cross_fields');
+    }
+
+    /**
+     * Return a phrase multi match type
+     *
+     * @return static
+     */
+    public static function phrase()
+    {
+        return new static('phrase');
+    }
+
+    /**
+     * Return a phrase_prefix multi match type
+     *
+     * @return static
+     */
+    public static function phrasePrefix()
+    {
+        return new static('phrase_prefix');
+    }
+
+}
---
+++ test/Queries/MultiMatchTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Best\ElasticSearch\Hercules\Test\Queries;
+
+use Best\ElasticSearch\Hercules\Queries\MultiMatch;
+use Best\ElasticSearch\Hercules\Type\MultiMatchType;
+
+class MultiMatchTest extends \PHPUnit\Framework\TestCase
+{
+    public function testToArray()
+    {
+        $multiMatch = new MultiMatch('welcome');
+        $result = $multiMatch->tieBreaker(5.0)
+            ->fields(['cheese', 'blarg'])
+            ->type(MultiMatchType::bestFields())
+            ->toArray();
+
+        $this->assertEquals([
+            'multi_match' => [
+                'query' => 'welcome',
+                'fields' => ['cheese', 'blarg'],
+                'type' => 'best_fields',
+                'tie_breaker' => 5.0
+            ],
+        ], $result);
+    }
+}
